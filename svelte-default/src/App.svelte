d66016bda7cef61ab630de0b76992847c6462d3e
Jerickson Tomajin <tomajinj@gmail.com>
2023-01-30 22:03:19 +0800

    svelte-default (temp)

---
+++ svelte-default/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+
+		h1 {
+			color: red;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
+	}
+
+	
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
